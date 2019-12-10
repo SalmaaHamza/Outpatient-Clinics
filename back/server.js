6696ccdccced4c7a4e89e7532e6fc582f42074d8
SalmaaHamza <salmahamza108@gmail.com>
2019-12-10 04:44:14 +0200

    new files

--- back/server.js
+++ back/server.js
@@ -3,11 +3,20 @@ const path = require('path')
 const bodyParser=require('body-parser')
 const app = express()
 app.use(bodyParser.urlencoded({extended:false}))
+const viewsDirectory = path.join(__dirname,'../front/views')
+const publicDirectory = path.join(__dirname,'../front/public')
+app.set('views',viewsDirectory)
+app.set('view engine','hbs')
+app.use(express.static(publicDirectory))
+
+
 app.get('/',(req,res)=>{
     console.log(req)
-    res.sendFile(path.join(__dirname,'../','front','index.html'))
+    res.render(path.join(__dirname,'../front/views/home/index.hbs'))
 
-})
+}
+
+)
 app.listen(3000,() => {
     console.log('Running')
 })
