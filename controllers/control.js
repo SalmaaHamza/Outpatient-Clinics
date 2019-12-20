d54134ce73c2175f078763dd532a30ff929e076d
remonalbear <remonalbear522@gmail.com>
2019-12-20 17:58:19 +0200

    add html

--- controllers/control.js
+++ controllers/control.js
@@ -7,8 +7,4 @@ exports.mainroute=(req,res,next) => {
 
 }
 
-<<<<<<< HEAD:controllers/control.js
-=======
-}
 
->>>>>>> 12ae1288257f4c8e5c46201107eae2159bb42119:back/controllers/control.js
--- server.js
+++ server.js
@@ -14,17 +14,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 const viewsDirectory = path.join(__dirname,'../front/views')
 app.set('views',viewsDirectory)
 app.set('view engine','hbs')
-<<<<<<< HEAD:server.js
 app.use(express.static(__dirname+'/public/'))
-=======
-// app.use(express.static(publicDirectory))
-// var connectionmysql = mysql.createConnection({
-//   host:'localhost',
-//   user:'root',
-//   password:'mysql',
-//   database:'outpatient'
-// })
->>>>>>> 12ae1288257f4c8e5c46201107eae2159bb42119:back/server.js
 
 
 
